6259eadb700a1f96858669aed90710302577fa4d
JosephWk <32693978+JosephWk@users.noreply.github.com>
2019-05-15 12:27:24 +0800

    changes made

--- rational.cpp
+++ rational.cpp
@@ -9,221 +9,225 @@
 namespace ExactArithmetic
 {
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-Rational::Rational()
-{
-    num   = 0;
-    denom = 1;
-}
+	Rational::Rational()
+	{
+		num = 0;
+		denom = 1;
+	}
 
-Rational::Rational(long long int numerator, long long int denominator)
-{
-    num   = numerator;
-    denom = denominator;
-    normalise();
-}
+	Rational::Rational(long long int numerator, long long int denominator)
+	{
+		num = numerator;
+		denom = denominator;
+		normalise();
+	}
 
-Rational::Rational(int i)
-{
-    num   = i;
-    denom = 1;
-}
+	Rational::Rational(int i)
+	{
+		num = i;
+		denom = 1;
+	}
 
-Rational::Rational(double r)
-{
-    num   = static_cast<long long int>(r);
-    denom = 1;
-}
+	Rational::Rational(double r)
+	{
+		num = static_cast<long long int>(r);
+		denom = 1;
+	}
 
-Rational::Rational(const std::string & str)
-{
-    std::istringstream iss(str);
-    iss >> *this;
-    if (! iss.eof()) throw std::invalid_argument(str + " cannot be parsed as a Rational.");
-}
+	Rational::Rational(const std::string & str)
+	{
+		std::istringstream iss(str);
+		iss >> *this;
+		if (!iss.eof()) throw std::invalid_argument(str + " cannot be parsed as a Rational.");
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-bool Rational::operator==(const Rational & r) const
-{
-    return (num == r.num) && (denom == r.denom);
-}
+	bool Rational::operator==(const Rational & r) const
+	{
+		return (num == r.num) && (denom == r.denom);
+	}
 
-bool Rational::operator!=(const Rational & r) const
-{
-    return (num != r.num) && (denom != r.denom);
-}
+	bool Rational::operator!=(const Rational & r) const
+	{
+		return (num != r.num) && (denom != r.denom);
+	}
 
-/* Comparison Operators
- *
- *  a/b < c/d
- *  if
- *     b > 0 (class invariant),
- *     d > 0 (class invariant),
- *  and
- *     a*d < c*b
- */
+	/* Comparison Operators
+	 *
+	 *  a/b < c/d
+	 *  if
+	 *     b > 0 (class invariant),
+	 *     d > 0 (class invariant),
+	 *  and
+	 *     a*d < c*b
+	 */
 
-bool Rational::operator<(const Rational & r) const
-{
-    return num < r.num || denom < r.denom;
-}
+	bool Rational::operator<(const Rational & r) const
+	{
+		return num < r.num || denom < r.denom;
+	}
 
-bool Rational::operator>(const Rational & r) const
-{
-    return num > r.num || denom > r.denom;
-}
+	bool Rational::operator>(const Rational & r) const
+	{
+		return num > r.num || denom > r.denom;
+	}
 
-bool Rational::operator<=(const Rational & r) const
-{
-    return num <= r.num || denom <= r.denom;
-}
+	bool Rational::operator<=(const Rational & r) const
+	{
+		return num <= r.num || denom <= r.denom;
+	}
 
-bool Rational::operator>=(const Rational & r) const
-{
-    return num >= r.num || denom >= r.denom;
-}
+	bool Rational::operator>=(const Rational & r) const
+	{
+		return num >= r.num || denom >= r.denom;
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-/*  Arithmetic Operators
- *
- *  a/b + c/d = (a*d + b*c) / b*d
- *  a/b - c/d = (a*d - b*c) / b*d
- *  (a/b) * (c/d) = (a*c) / (b*d)
- *  (a/b) / (c/d) = (a*d) / (b*c)  (division by zero throws a DivideByZeroError)
- */
+	/*  Arithmetic Operators
+	 *
+	 *  a/b + c/d = (a*d + b*c) / b*d
+	 *  a/b - c/d = (a*d - b*c) / b*d
+	 *  (a/b) * (c/d) = (a*c) / (b*d)
+	 *  (a/b) / (c/d) = (a*d) / (b*c)  (division by zero throws a DivideByZeroError)
+	 */
 
-Rational Rational::operator+(const Rational & r) const
-{
-	return Rational(num * r.denom + r.num*denom, denom * r.denom);
-}
+	Rational Rational::operator+(const Rational & r) const
+	{
+		return Rational(num * r.denom + r.num*denom, denom * r.denom);
+	}
 
-Rational Rational::operator-(const Rational & r) const
-{
-	return Rational((num *r.denom) - (r.num*denom), denom * r.denom);
-}
+	Rational Rational::operator-(const Rational & r) const
+	{
+		return Rational((num *r.denom) - (r.num*denom), denom * r.denom);
+	}
 
-Rational Rational::operator*(const Rational & r) const
-{
-    return Rational(num * r.num, denom * r.denom);
-}
+	Rational Rational::operator*(const Rational & r) const
+	{
+		return Rational(num * r.num, denom * r.denom);
+	}
 
-Rational Rational::operator/(const Rational & r) const
-{
-	return Rational(num * r.denom, denom*r.num);
-}
+	Rational Rational::operator/(const Rational & r) const
+	{
+		return Rational(num * r.denom, denom*r.num);
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-Rational & Rational::operator+=(const Rational & r)
-{
-	(num * r.denom) + (r.num*denom);
+	Rational & Rational::operator+=(const Rational & r)
+	{
+		num *= r.denom;
+		num += r.num * denom;
+		denom *= r.denom;
+		// denom += r.denom;
+		normalise();
+		return *this;
+	}
 
-	//num   += r.num;
-   // denom += r.denom;
-    normalise();
-    return *this;
-}
+	Rational & Rational::operator-=(const Rational & r)
+	{
 
-Rational & Rational::operator-=(const Rational & r)
-{
-	(num *r.denom) - (r.num*denom);
-	denom * r.denom;
+		num *= r.denom;
+		num = num - (r.num * denom);
+		denom *= r.denom;
 
-   // num   -= r.num;
-  //  denom -= r.denom;
-    normalise();
-    return *this;
-}
+		// num   -= r.num;
+	    //  denom -= r.denom;
+		normalise();
+		return *this;
+	}
 
-Rational & Rational::operator*=(const Rational & r)
-{
-    num   *= r.num;
-    denom *= r.denom;
-    normalise();
-    return *this;
-}
+	Rational & Rational::operator*=(const Rational & r)
+	{
+		num *= r.num;
+		denom *= r.denom;
+		normalise();
+		return *this;
+	}
 
-Rational & Rational::operator/=(const Rational & r)
-{
-    num   /= r.num;
-    denom /= r.denom;
-    normalise();
-    return *this;
-}
+	Rational & Rational::operator/=(const Rational & r)
+	{
+		num *= r.denom;
+		denom *= r.num;
+		//   num   /= r.num;
+		//  denom /= r.denom;
+		normalise();
+		return *this;
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-Rational Rational::abs(const Rational & r)
-{
-    return Rational(r.num,std::abs(r.denom));
-}
+	Rational Rational::abs(const Rational & r)
+	{
+		return Rational(std::abs(r.num), std::abs(r.denom));
+	}
 
-Rational Rational::negate(const Rational & r)
-{
-    return Rational(-(r.num),-(r.denom));
-}
+	Rational Rational::negate(const Rational & r)
+	{
+		return Rational(-(r.num), r.denom);
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-void Rational::normalise()
-{
-    if (denom == 0)
-    {
-        throw(DivideByZeroError());
-    }
+	void Rational::normalise()
+	{
+		if (denom == 0)
+		{
+			throw(DivideByZeroError());
+		}
 
-    if (denom < 0)
-    {
-        num   = -num;
-        denom = -denom;
-    }
+		if (denom < 0)
+		{
+			num = -num;
+			denom = -denom;
+		}
 
-    if ((num == 0) || (denom == 1))
-    {
-        denom = 1;
-    }
-    else
-    {
-        long long int factor = gcd(num,denom);
-        num   /= factor;
-        denom /= factor;
-    }
-}
+		if ((num == 0) || (denom == 1))
+		{
+			denom = 1;
+		}
+		else
+		{
+			long long int factor = gcd(num, denom);
+			num /= factor;
+			denom /= factor;
+		}
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
-std::ostream & operator<<(std::ostream & output, const Rational & r)
-{
-    if (r.denom == 1)
-    {
-        output << r.num;
-    }
-    else
-    {
-        output << r.num << '/' << r.denom;
-    }
-    return output;
-}
+	std::ostream & operator<<(std::ostream & output, const Rational & r)
+	{
+		if (r.denom == 1)
+		{
+			output << r.num;
+		}
+		else
+		{
+			output << r.num << '/' << r.denom;
+		}
+		return output;
+	}
 
-std::istream & operator>>(std::istream & input, Rational & r)
-{
-    input >> r.num;
-    if (input.peek() == '/')
-    {
-        input.ignore(1);
-        input >> r.denom;
-    }
-    else
-    {
-        r.denom = 1;
-    }
-    r.normalise();
-    return input;
-}
+	std::istream & operator>>(std::istream & input, Rational & r)
+	{
+		input >> r.num;
+		if (input.peek() == '/')
+		{
+			input.ignore(1);
+			input >> r.denom;
+		}
+		else
+		{
+			r.denom = 1;
+		}
+		r.normalise();
+		return input;
+	}
 
-////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////
 
 } // End of namespace ExactArithmetic
--- unit_test.cpp
+++ unit_test.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include "rational.h"
+#include "dividebyzeroerror.h"
 
 using namespace ExactArithmetic;
 
@@ -11,65 +12,63 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-BOOST_AUTO_TEST_CASE(normalise_test)
-{
-	Rational n = Rational(3, 9);
-	BOOST_CHECK_EQUAL(n, Rational(1, 3));
-}
 
-BOOST_AUTO_TEST_CASE(addtion_resut_test) {
+
+BOOST_AUTO_TEST_CASE(addtion_result_test) {
 	Rational x = Rational(4, 9);
 	Rational y = Rational(1, 3);
 	BOOST_CHECK_EQUAL((x + y), Rational(7 , 9));
 }
 
-BOOST_AUTO_TEST_CASE(substraction_resut_test) {
+BOOST_AUTO_TEST_CASE(substraction_result_test) {
 	Rational x = Rational(4, 9);
 	Rational y = Rational(1, 3);
 	BOOST_CHECK_EQUAL((x - y), Rational(1,9));
 }
 
-BOOST_AUTO_TEST_CASE(multiplication_resut_test) {
+BOOST_AUTO_TEST_CASE(multiplication_result_test) {
 	Rational x = Rational(4, 9);
 	Rational y = Rational(1, 3);
 	BOOST_CHECK_EQUAL((x * y), Rational(4,27));
 }
 
-BOOST_AUTO_TEST_CASE(division_resut_test) {
+BOOST_AUTO_TEST_CASE(division_result_test) {
 	Rational x = Rational(4, 9);
 	Rational y = Rational(1, 3);
 	BOOST_CHECK_EQUAL((x / y), Rational(4,3));
 }
 
+
+
 // Compound Assignment operators
 BOOST_AUTO_TEST_CASE(addtion_compound_assign_test) {
 	Rational y = Rational(1, 3);
-	Rational z = Rational(1, 3);
+	Rational z = Rational(2, 4);
 	z += y;
-	BOOST_CHECK_EQUAL(z, Rational(2, 3));
+	BOOST_CHECK_EQUAL(z, Rational(5, 6));
 }
 
 BOOST_AUTO_TEST_CASE(substration_compound_assign_test) {
-	Rational y = Rational(1, 3);
-	Rational z;
+	Rational z = Rational(1, 4);
+	Rational y = Rational(2, 3);
 	z-= y;
-	BOOST_CHECK_EQUAL(z, Rational(0));
+	BOOST_CHECK_EQUAL(z, Rational(-5,12));
 }
 BOOST_AUTO_TEST_CASE(multiplication_compound_assign_test) {
-	Rational z;
-	z *= Rational(1, 3);
+	Rational y = Rational(1, 3);
+	Rational z = Rational(1, 3);
+	z *= y;
 	BOOST_CHECK_EQUAL(z, Rational(1, 9));
 }
 BOOST_AUTO_TEST_CASE(division_compound_assign_test) {
-	Rational y = Rational(1, 3);
-	Rational z;
+	Rational z = Rational(5, 6);
+	Rational y = Rational(6, 3);
 	z /= y;
-	BOOST_CHECK_EQUAL(z, Rational(1));
+	BOOST_CHECK_EQUAL(z, Rational(5,12));
 }
 
 
 
-// Comparison operators
 
 BOOST_AUTO_TEST_CASE(more_than_test) {
 	Rational a = Rational(2, 3);
@@ -107,26 +106,49 @@ BOOST_AUTO_TEST_CASE(not_equal_to_test) {
 
 
 
+BOOST_AUTO_TEST_CASE(normalise_test)
+{
+	Rational n = Rational(3, 9);
+	BOOST_CHECK_EQUAL(n, Rational(1, 3));
+}
 
-// Negation
+
+BOOST_AUTO_TEST_CASE(throw_divideByZeroError) {
+	BOOST_CHECK_THROW(Rational(2, 4) / 0, DivideByZeroError);
+}
+
+
+
+
+BOOST_AUTO_TEST_CASE(negate_on_positive_test) {
+	Rational a = Rational::negate(Rational(1, 3));
+	Rational b = Rational(-1,3);
+	BOOST_CHECK_EQUAL(a, b);
+}
+
+BOOST_AUTO_TEST_CASE(negate_on_negative_test) {
+	Rational a = Rational::negate(Rational(-1, 3));
+	Rational b = Rational(1,3);
+	BOOST_CHECK_EQUAL(a, b);
+}
+
+BOOST_AUTO_TEST_CASE(abs_on_negative_test) {
+	Rational a = Rational::abs(Rational(-1, 3));
+	Rational b = Rational(1,3);
+	BOOST_CHECK_EQUAL(a, b);
+}
+
+
+BOOST_AUTO_TEST_CASE(abs_on_positive_test) {
+	Rational a = Rational::abs(Rational(1, 3));
+	Rational b = Rational(1,3);
+	BOOST_CHECK_EQUAL(a, b);
+}
+
+
 
 
 /*
-BOOST_AUTO_TEST_CASE(division_resut_test) {
-	Rational a = negate(Rational(1, 3));
-	Rational b = Rational(-1, -3);
-	BOOST_CHECK_EQUAL(a, b);
-
-}
-
-BOOST_AUTO_TEST_CASE(division_resut_test) {
-	BOOST_CHECK_EQUAL((negate(negativeR), );
-}
-
-
-
-
-
 BOOST_AUTO_TEST_CASE(cin_operator_overload_test)) {
 	
 }
